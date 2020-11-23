0ecdfbde654ecafe1f01e2aa235c45505f2684d6
Karakalchev Roman <rombl4_83@mail.ru>
2020-11-23 21:16:27 +0700

    Клиентская часть

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,2 +1,27 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        int c;
+
+        System.out.print("Welcome! Messenger 1.0 client");
+
+        // TODO сделать загрузку параметров изконфигурационного файла
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("IP:");
+        String ip = scanner.nextLine();
+
+        int port = 43;
+
+        Socket socket = new Socket(ip, port);
+
+        InputStream inMsg = socket.getInputStream();
+        OutputStream outMsg = socket.getOutputStream();
+
+        while ((c = inMsg.read()) != -1) {
+            System.out.print((char) c);
+        }
+    }
 }
