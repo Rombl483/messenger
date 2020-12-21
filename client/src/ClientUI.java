2d3fb1f336e046c16eea1d389059d796d85059f1
Геннадий <kirin_gg@ngs.ru>
2020-12-21 16:24:33 +0700

    Add ClienUA form

---
+++ client/src/ClientUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ClientUI {
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JTextField host;
+    private JButton connectButton;
+    private JTextField textField1;
+    private JButton sendButton;
+}
